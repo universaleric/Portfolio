ec0f3020e2b90104079295b339624a9e46554403
Eric Hay <ericdanielhay@yahoo.com>
2021-06-22 21:52:11 -0500

    Added fade effects to name and portrait and updated structure of about me page.

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -2,25 +2,29 @@ import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import Fade from "react-reveal/Fade";
+import Portrait from "../img/Portrait.png";
 
 function About() {
   return (
     <div>
-      <Container>
+      <Container style={{ marginTop: 50 }}>
         <Row>
-          <Col size="md-12">
-            <h1>about me.</h1>
+          <Col size="md-8">
+            <Fade left>
+              <p className="about">
+                Hello! I'm a 30 year old web developer hailing from the good ol'
+                state of Texas. I love working with others and collaborating to
+                make the most of our collective talent. I think the little
+                things matter and can make or break the feel and function of a
+                website. Oh, and I have a hedgehog as my assistant.
+              </p>
+            </Fade>
           </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <p>
-              Hello! I'm a 30 year old web developer hailing from the good ol'
-              state of Texas. I love working with others and collaborating to
-              make the most of our collective talent. I think the little things
-              matter and can make or break the feel and function of a website.
-              Oh, and I have a hedgehog as my assistant.
-            </p>
+          <Col className="portrait" size="md-4">
+            <Fade>
+              <img src={Portrait} alt="placeholder" />
+            </Fade>
           </Col>
         </Row>
       </Container>
--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import Fade from 'react-reveal/Fade';
 import Portrait from "../img/Portrait.png";
 
 function Home() {
@@ -10,13 +11,17 @@ function Home() {
       <Container style={{ marginTop: 50 }}>
         <Row>
           <Col size="md-8">
+            <Fade left>
             <p className="home">Eric Hay</p>
+            </Fade>
           </Col>
           <Col className="portrait" size="md-4">
+          <Fade>
             <img
               src={Portrait}
               alt="placeholder"
             />
+            </Fade>
           </Col>
         </Row>
       </Container>
