0a2966f088ee07d3cc06af54149747029c5ef185
Eric Hay <ericdanielhay@yahoo.com>
2021-06-25 09:05:34 -0500

    Added icons for skills.

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,35 +2,38 @@ import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import Spin from "react-reveal/Spin";
 
 function Skills() {
   return (
     <div>
       <Container style={{ marginTop: 30 }}>
         <Row>
-          <Col size="md-12">
-            <h1>skills.</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
+          <Col size="md-6">
             <ul>
-              <li>HTML</li>
+              <li>
+                HTML{" "}
+                <Spin>
+                  <i class="fas fa-star"></i>
+                </Spin>
+                <Spin>
+                  <i class="fas fa-star"></i>
+                </Spin>
+                <Spin>
+                  <i class="fas fa-star"></i>
+                </Spin>
+              </li>
               <li>CSS</li>
               <li>Javascript</li>
               <li>Python</li>
-              <li>&#9733 &#9733 &#9733 &#9734 &#9734</li>
-              <li>&#9733 &#9733 &#9733 &#9734 &#9734</li>
-              <li>&#9733 &#9733 &#9733 &#9734 &#9734</li>
-              <li>&#9733 &#9733 &#9734 &#9734 &#9734</li>
+            </ul>
+          </Col>
+          <Col size="md-6">
+            <ul>
               <li>SQL</li>
               <li>React</li>
               <li>Photoshop</li>
               <li>Microsoft Office</li>
-              <li>&#9733 &#9733 &#9734 &#9734 &#9734</li>
-              <li>&#9733 &#9733 &#9734 &#9734 &#9734</li>
-              <li>&#9733 &#9733 &#9733 &#9733 &#9733</li>
-              <li>&#9733 &#9733 &#9733 &#9733 &#9733</li>
             </ul>
           </Col>
         </Row>
