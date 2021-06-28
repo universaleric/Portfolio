666e3d55b451a5dff7a2f7ddeb02f7b4b1557a8c
Eric Hay <ericdanielhay@yahoo.com>
2021-06-28 09:46:46 -0500

    Adjusted sizing and position for the skills, projects, and contact tabs.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,26 +12,26 @@ function Contact() {
         <Row>
           <Col size="md-8">
             <Fade left cascade>
-              <ul className="list">
-                <li>
+              <ul className="list listContact">
+                <li className="contact">
                   <i class="fas fa-phone"></i> (972).955.2176
                 </li>
-                <li>
+                <li className="contact">
                   <i className="fas fa-envelope"></i> ericdanielhay@yahoo.com
                 </li>
-                <li>
+                <li className="contact">
                   <i class="fab fa-github"></i>{" "}
                   <a href="https://www.github.com/universaleric">
                     Github Account
                   </a>
                 </li>
-                <li>
+                <li className="contact">
                   <i className="fab fa-linkedin"></i>{" "}
                   <a href="https://www.linkedin.com/in/eric-hay-06b03371">
                     LinkedIn Profile
                   </a>
                 </li>
-                <li>
+                <li className="contact">
                   <i className="fas fa-file"></i>{" "}
                   <a href="https://drive.google.com/file/d/1AgcHLaYvDNc_X-yhcfJyPAQp51-1tN9M/view?usp=sharing">
                     Resume
